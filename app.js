486d9e3a550c45c7fee45260831f88f135133291
のびすけ <rsksound@gmail.com>
2018-02-18 15:57:51 +0900

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname+'/'));
+
+const PORT = process.env.PORT || 3000;
+
+app.post('/', async (req, res) => {
+    console.log(req.body); //PayPalから送られて来る情報の確認    
+
+    const response = {
+        message: 'Success!'
+    };
+    res.end(JSON.stringify(response));
+});
+
+app.listen(PORT);
+console.log(`listening on *:${PORT}`);
