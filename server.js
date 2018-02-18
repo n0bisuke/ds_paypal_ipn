007d29f562b946db35db5b4b77102ecc3eafb25c
のびすけ <rsksound@gmail.com>
2018-02-18 18:16:13 +0900

    update:

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const axios = require('axios');
+const VM_URL = process.env.MAIN_HOST || '';
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname+'/'));
@@ -11,8 +13,21 @@ const PORT = process.env.PORT || 4003;
 
 app.get('/', async (req, res) => res.sendFile(__dirname+'index.html'));
 
-app.post('/', async (req, res) => {
-    console.log(req.body); //PayPalから送られて来る情報の確認    
+app.post('/ipn_webapps', async (req, res) => {
+    console.log(req.body); //PayPalから送られて来る情報の確認
+    const options = {
+        method: 'post',
+        baseURL: VM_URL,
+        url: '/ipn_vm',
+        data: req.body,
+    };
+    await axios.request(options);//VMに投げる
+    const response = {message: 'Success!'};
+    res.end(JSON.stringify(response));
+});
+
+app.post('/ipn_vm', async (req, res) => {
+    console.log(req.body); //PayPalから送られて来る情報の確認
 
     const response = {
         message: 'Success!'
